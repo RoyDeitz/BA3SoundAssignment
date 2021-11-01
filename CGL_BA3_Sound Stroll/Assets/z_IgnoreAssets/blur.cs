d7c4c8df0a22a83a00e622b3d1eb7e99f5503835
RoyDevashis <roydeitz@gmail.com>
2021-11-01 15:00:42 +0100

    deliverablesOfFirstSubmission

---
+++ CGL_BA3_Sound Stroll/Assets/z_IgnoreAssets/blur.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class blur : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Ray raycast;
+    RaycastHit hit;
+    bool isHit;
+    float hitDistance;
+
+    public PostProcessVolume volume;
+
+    DepthOfField depthOfField;
+
+    [Range(1, 10)]
+    public float focusSpeed;
+    public float maxFocusDistance;
+
+    void Start()
+    {
+        volume.profile.TryGetSettings(out depthOfField);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        raycast = new Ray(transform.position, transform.forward * maxFocusDistance);
+
+        isHit = false;
+
+        if (Physics.Raycast(raycast, out hit, maxFocusDistance) && (hit.collider.tag != "Player"))
+        {
+            isHit = true;
+            hitDistance = Vector3.Distance(transform.position, hit.point);
+        }
+        else
+        {
+            if (hitDistance < maxFocusDistance)
+            {
+                hitDistance++;
+            }
+        }
+
+        SetFocus();
+    }
+
+    void SetFocus()
+    {
+        depthOfField.focusDistance.value = Mathf.Lerp(depthOfField.focusDistance.value, hitDistance, Time.deltaTime * focusSpeed);
+    }
+
+}
+
