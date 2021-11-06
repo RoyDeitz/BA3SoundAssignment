6f1bde3f287623bcb7a63da57f9b860ea379799e
RoyDevashis <roydeitz@gmail.com>
2021-11-06 21:24:57 +0100

    Ambience switching scripts added

---
+++ CGL_BA3_Sound Stroll/Assets/Scripts/AmbienceSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceSwitcher : MonoBehaviour
+{
+    public bool IsWindy;
+    public bool IsBackyard;
+    public GameObject windyAmbienceObj;
+    public GameObject backwardAmbienceObj;
+
+    private void Update()
+    {
+        if (IsWindy)
+        {
+            IsBackyard = false;
+            if (!windyAmbienceObj.activeSelf)
+            {
+                windyAmbienceObj.SetActive(true);
+            }
+        }
+        else 
+        {
+            windyAmbienceObj.SetActive(false);
+        }
+        if (IsBackyard)
+        {
+            IsWindy = false;
+            if (!backwardAmbienceObj.activeSelf)
+            {
+                backwardAmbienceObj.SetActive(true);
+            }
+        }
+        else 
+        {
+            backwardAmbienceObj.SetActive(false);
+        }
+    }
+}
---
+++ CGL_BA3_Sound Stroll/Assets/Scripts/AmbienceTrigger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceTrigger : MonoBehaviour
+{
+    AmbienceSwitcher ambienceSwitcher;
+
+    private void Start()
+    {
+        ambienceSwitcher = FindObjectOfType<AmbienceSwitcher>();
+    }
+    public enum AmbienceMode 
+    {
+    WINDY,
+    BACKYARD
+    }
+    public AmbienceMode ambienceMode;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (ambienceMode == AmbienceMode.WINDY)
+            {
+                if (ambienceSwitcher != null)
+                {
+                    ambienceSwitcher.IsWindy = true;
+                }
+            }
+            else if (ambienceMode == AmbienceMode.BACKYARD)
+            {
+                if (ambienceSwitcher != null)
+                {
+                    ambienceSwitcher.IsBackyard = true;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (ambienceMode == AmbienceMode.WINDY)
+            {
+                if (ambienceSwitcher != null)
+                {
+                    ambienceSwitcher.IsWindy = false;
+                }
+            }
+            else if (ambienceMode == AmbienceMode.BACKYARD)
+            {
+                if (ambienceSwitcher != null)
+                {
+                    ambienceSwitcher.IsBackyard = false;
+                }
+            }
+        }
+    }
+}
